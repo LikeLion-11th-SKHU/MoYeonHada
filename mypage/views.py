1b35ba0fec623297ba890f7904f702cdda72f63b
Rommmu <tonty1222@naver.com>
2023-08-08 19:16:30 +0900

    Revert "Youngbeen"

--- mypage/views.py
+++ mypage/views.py
@@ -1,32 +1,3 @@
 from django.shortcuts import render
-from .models import Bookmark
 
 # Create your views here.
-
-def base(request):
-    return render(request, 'base.html')
-
-def mypage(request):
-    return render(request, 'mypage.html')
-
-def bookmark(request):
-    return render(request, 'bookmark.html')
-
-def list(request):
-    return render(request, 'list.html')
-
-
-
-# def BookmarkList(ListView):
-#     model = Bookmark
-
-# def BookmarkCreate(CreateView):
-#     model = Bookmark
-#     fields = ['site_name', 'url', 'contents']
-#     template_name_suffix = '_create'
-#     success_url = '/'
-
-# def BookmarkUpdate(UpdateView):
-#     model = Bookmark
-#     fields = ['site_name', 'url', 'contents']
-#     template
--- oneday/admin.py
+++ oneday/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import OnedayApply, OnedayRecruit
-# Register your models here.
 
-admin.site.register(OnedayRecruit)
-admin.site.register(OnedayApply)
+# Register your models here.
