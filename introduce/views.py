7eed69222fd5d025ad5e4da1a8105de6e2cc2632
Rommmu <tonty1222@naver.com>
2023-08-08 18:18:17 +0900

    Revert "Modify homeApp & Create introduce Developers"

--- introduce/views.py
+++ introduce/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def introduce_main(request):
     return render(request, 'introduce_main.html')
-
-def introduce_developer(request):
-    return render(request, 'introduce_developer.html')
