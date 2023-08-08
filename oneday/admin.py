c993cafd1089ee3c9e3c451a1cdfbd08b48e5d5b
Rommmu <tonty1222@naver.com>
2023-08-08 23:40:35 +0900

    Delete admin.py code

--- oneday/admin.py
+++ oneday/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import OnedayApply, OnedayRecruit
-# Register your models here.
-
-admin.site.register(OnedayRecruit)
-admin.site.register(OnedayApply)
