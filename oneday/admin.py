a00165d62be4757d4a75fb3d2575b4fa330fd33b
kimyongwook98 <rladyddnr0209@naver.com>
2023-08-08 18:34:28 +0900

    modify oneday

--- oneday/admin.py
+++ oneday/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import OnedayApply, OnedayRecruit
 # Register your models here.
+
+admin.site.register(OnedayRecruit)
+admin.site.register(OnedayApply)
