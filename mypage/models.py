6b65c19858953517e5753bd2232d69a7e329bd9f
CINI <shinhee1241@gmail.com>
2023-08-08 18:09:34 +0900

    shinhee 폴리퀘

--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) #현 계정의 사용자를 가져올 수 ㅇ
+    nickname = models.CharField(max_length = 64)
+    profile_photo = modelsImageField(blank = True)
+
+
--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+def mypage(request):
+    return render(request, 'mypage.html')
+
+def bookmark(request):
+    return render(request, 'bookmark.html')
+
+def list(request):
+    return render(request, 'list.html')
