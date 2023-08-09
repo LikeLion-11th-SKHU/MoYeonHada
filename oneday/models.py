94a0b6a67d3a0d84b27124b087077898c1213d44
kimyongwook98 <rladyddnr0209@naver.com>
2023-08-09 16:52:03 +0900

    yong

--- oneday/models.py
+++ oneday/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
 
 # 유저 모델
 class Oneday(models.AbstractUser):
